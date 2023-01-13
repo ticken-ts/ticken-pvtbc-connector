29a8dc470ff108750abe1d32664014613bf2c2d9
facundotorraca <facundo@innovid.com>
2023-01-13 03:11:11 -0300

    update chain-models

--- chain-models/event.go
+++ chain-models/event.go
@@ -4,17 +4,20 @@ import (
 	"time"
 )
 
-type Section struct {
-	Name             string `json:"name"`
-	EventID          string `json:"event_id"`
-	TotalTickets     int    `json:"total_tickets"`
-	RemainingTickets int    `json:"remaining_tickets"`
+type Event struct {
+	EventID  string     `json:"event_id"`
+	Name     string     `json:"name"`
+	Date     time.Time  `json:"date"`
+	Sections []*Section `json:"sections"`
+
+	// identity of the event and auditory
+	MSPID             string `json:"msp_id"`
+	OrganizerUsername string `json:"organizer_username"`
 }
 
-type Event struct {
-	EventID        string    `json:"event_id"`
-	Name           string    `json:"name"`
-	Date           time.Time `json:"date"`
-	Sections       []Section `json:"sections"`
-	OrganizationID string    `json:"organization_id"`
+type Section struct {
+	Name         string  `json:"name"`
+	TicketPrice  float64 `json:"ticket_price"`
+	TotalTickets int     `json:"total_tickets"`
+	SoldTickets  int     `json:"sold_tickets"`
 }
